1bcee2dd1ac74cf78cc12d4fd7e58a394db54790
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2024-12-18 22:50:51 -0300

    adicionar botoes

--- src/components.js
+++ src/components.js
@@ -1,70 +1,88 @@
 import { listaPratos, valorComanda } from "./pratos.js";
 
 class CardPrato {
-    // variáveis públicas.
+  // variáveis públicas.
 
-    nodeElem = document.createElement('div');
-    tituloElem = document.createElement('h2');    
-    tituloContainer = document.createElement('div');
-    descricaoElem = document.createElement('p');
-    precoElem = document.createElement('span');
-    btnContainer = document.createElement('div');
-    qtdElem = document.createElement('span');
-    pedirBtn = document.createElement('button');
+  nodeElem = document.createElement("div");
+  tituloElem = document.createElement("h2");
+  tituloContainer = document.createElement("div");
+  descricaoElem = document.createElement("p");
+  precoElem = document.createElement("span");
+  btnContainer = document.createElement("div");
+  qtdElem = document.createElement("span");
+  pedirBtn = document.createElement("button");
 
+  qtd = 0;
+  preco = 0;
 
-    qtd = 0
-    preco = 0
+  // construtor
+  constructor(info) {
+    this.tituloElem.innerText = info.titulo;
+    this.tituloElem.classList.add("titulo__head");
 
-    // construtor
-    constructor(info) {
-        this.tituloElem.innerText = info.titulo;
-        this.tituloElem.classList.add('titulo__head')
-        
-        this.precoElem.innerText = info.preco;
-        this.precoElem.classList.add('titulo__preco');
-        
+    this.precoElem.innerText = info.preco;
+    this.precoElem.classList.add("titulo__preco");
 
-        this.preco = parseFloat(info.preco);
-        
-        this.tituloContainer.classList.add('titulo');
-        this.tituloContainer.append(this.tituloElem, this.precoElem);
+    this.preco = parseFloat(info.preco);
 
-        this.descricaoElem.innerText = info.descricao;
-        this.descricaoElem.classList.add('descricao');
-        
+    this.tituloContainer.classList.add("titulo");
+    this.tituloContainer.append(this.tituloElem, this.precoElem);
 
-        this.qtdElem.classList.add('pedidos__qtd');
-        this.qtdElem.innerText = 0;
-        
-        this.nodeElem.classList.add('prato');
-        
-        this.pedirBtn.innerText = "Pedir";
-        this.pedirBtn.classList.add("pedidos__btn");
-        
-        this.pedirBtn.onclick = () => {
-            this.qtd += 1; 
-            this.qtdElem.innerText = this.qtd;
-            valorComanda();
-        };
+    this.descricaoElem.innerText = info.descricao;
+    this.descricaoElem.classList.add("descricao");
 
-        this.btnContainer.classList.add("pedidos");
-        
-        this.btnContainer.append(this.pedirBtn,this.qtdElem);
-        this.nodeElem.append(this.tituloContainer, this.descricaoElem,this.btnContainer);
-    }
+    this.qtdElem.classList.add("pedidos__qtd");
+    this.qtdElem.innerText = 0;
 
-    get container() {
-        return this.nodeElem;
-    }
+    this.nodeElem.classList.add("prato");
 
-    get subtotal() {
-        return this.qtd * this.preco;
-    }
+    this.pedirBtn.innerText = "Pedir";
+    this.pedirBtn.classList.add("pedidos__btn");
 
+    this.pedirBtn.onclick = () => {
+      this.qtd += 1;
+      this.qtdElem.innerText = this.qtd;
+      valorComanda();
+    };
+
+    this.diminuirBtn.innerText = "Diminuir";
+    this.diminuirBtn.classList.add("pedidos__btn");
+    this.diminuirBtn.onclick = () => {
+      if (this.qtd > 0) {
+        this.qtd -= 1;
+        this.qtdElem.innerText = this.qtd;
+        valorComanda();
+      }
+    };
+
+    this.zerarBtn.innerText = "Zerar";
+    this.zerarBtn.classList.add("pedidos__btn");
+    this.zerarBtn.onclick = () => {
+      this.qtd = 0;
+      this.qtdElem.innerText = this.qtd;
+      valorComanda();
+    };
+
+    this.btnContainer.classList.add("pedidos");
+
+    this.btnContainer.append(this.pedirBtn, this.qtdElem);
+    this.nodeElem.append(
+      this.tituloContainer,
+      this.descricaoElem,
+      this.btnContainer
+    );
+  }
+
+  get container() {
+    return this.nodeElem;
+  }
+
+  get subtotal() {
+    return this.qtd * this.preco;
+  }
 }
 
 export function criarCardPrato(info) {
-    const cardObjeto = new CardPrato(info);
-    return cardObjeto;
+  const cardObjeto = new CardPrato(info);
+  return cardObjeto;
 }
